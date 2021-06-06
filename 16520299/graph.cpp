d841a60678cdaa88aaa9644844ff121f7186f539
malikrafsan <pro.malikakbar2357@gmail.com>
2021-06-06 18:52:40 +0700

    implement destructor

--- 16520299/graph.cpp
+++ 16520299/graph.cpp
@@ -5,10 +5,11 @@
 Graph::Graph(int V) {
     this->V = V;
     adj = new std::list<int>[V];
-    std::cout << "TES" << std::endl;
 }
 
-Graph::~Graph() {}
+Graph::~Graph() {
+    delete adj;
+}
 
 void Graph::addEdge(int v, int w) {
     adj[v].push_back(w);
